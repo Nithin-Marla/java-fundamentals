a96c4ec00efaae743789024d3e7777a813bdb175
Nithin-Marla <marlanithin288@gmail.com>
2026-01-20 09:03:52 +0530

    Reverse a number

---
+++ src/com/stschool/java/fundamentals/ReverseOfANum.java
@@ -0,0 +1,17 @@
+package com.stschool.java.fundamentals;
+
+import java.util.Scanner;
+
+public class ReverseOfANum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=0;
+        while(n>0){
+            int r=n%10;
+            m=m*10+r;
+            n=n/10;
+        }
+        System.out.println(m);
+    }
+}
