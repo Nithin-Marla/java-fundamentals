570e8bd4fe37b4d2a8593b3bd91d98e0adec2bf6
Nithin-Marla <marlanithin288@gmail.com>
2026-01-22 14:31:44 +0530

    data types with examples

---
+++ src/com/stschool/java/fundamentals/Primitives.java
@@ -0,0 +1,59 @@
+package com.stschool.java.fundamentals;
+
+public class Primitives {
+    public static void main(String []args){
+        byte age, batteryCharge, daysInAWeek, shirtSize;
+        age = 21;
+        batteryCharge = 50;
+        daysInAWeek = 7;
+        shirtSize = 40;
+        System.out.println("age = " + age);
+        System.out.println("Battery charge = " + batteryCharge);
+        System.out.println("Days In a Week = " + daysInAWeek);
+        System.out.println("shirt size = " + shirtSize);
+
+        short year, totalStudents, stepCount, electricityUnits;
+        year = 2026;
+        totalStudents = 12000;
+        stepCount = 10000;
+        electricityUnits = 850;
+        System.out.println("Year = " + year);
+        System.out.println("StepCount = " + stepCount);
+        System.out.println("total Students = " + totalStudents);
+        System.out.println("electricity Units = " + electricityUnits);
+
+        int salary, pincode, bikePrice, weddingBudget;
+        salary = 50000;
+        pincode = 501505;
+        bikePrice = 180000;
+        weddingBudget = 500000;
+        System.out.println("Salary = " + salary);
+        System.out.println("pincode = " + pincode);
+        System.out.println("Bike price = " + bikePrice);
+        System.out.println("Wedding Budget = " + weddingBudget);
+
+        long phoneNumber, aadharNumber, accountNumber, indianPopulation;
+        phoneNumber = 944027456L;
+        aadharNumber = 629075876352l;
+        accountNumber = 163101004188552l;
+        indianPopulation = 1400000000l;
+        System.out.println("phone number = " + phoneNumber);
+        System.out.println("Aadhar number = " + aadharNumber);
+        System.out.println("Account number = " + accountNumber);
+        System.out.println("India population = " + indianPopulation);
+
+        float humanWeight, temperature, humanHeight, distance;
+        humanWeight = 73.5F;
+        temperature = 26.7F;
+        humanHeight = 5.10F;
+        distance = 4.5F;
+        System.out.println("weight = " + humanWeight);
+        System.out.println("My Height = " + humanHeight);
+        System.out.println("Temperature = " + temperature);
+        System.out.println("distance in km = " + distance);
+
+
+
+
+    }
+}
