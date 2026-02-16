818fbe3b60eb3e0bb560a05df589653f9b5898e6
Nithin-Marla <marlanithin288@gmail.com>
2026-02-17 00:46:18 +0530

    add a program to find factors of a number

--- src/com/stschool/java/programs/level3/FactorsOfN.java
+++ src/com/stschool/java/programs/level3/FactorsOfN.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class FactorsOfN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int number = sc.nextInt();
         int i = 1;
-        while (i <= n) {
-            if (n % i == 0)
+        while (i <= number) {
+            if (number % i == 0)
                 System.out.println(i);
             i++;
         }
